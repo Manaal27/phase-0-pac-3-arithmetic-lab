16a591dac503ccf675997b8ad075d0b8e8868bec
manaal <manaal.basabra@student.moringaschool.com>
2022-08-04 16:20:38 +0300

    Completed assignment

---
+++ index.js
@@ -0,0 +1,36 @@
+function add(a) {
+    return a++;
+}
+function subtract(a) {
+    return a--;
+}
+function multiply(a) {
+    return a*b;
+}
+function divide(a, b) {
+    return a/b;
+}
+function add(a, b) {
+    return a + b;
+}
+function subtract(a, b) {
+    return a - b;
+}
+function multiply(a, b) {
+    return a * b;
+}
+function increment (n) {
+    return ++ n;
+}
+function decrement (n) {
+    return -- n;
+}
+
+function makeInt (n) {
+    return parseInt(n, 10);
+}
+
+function preserveDecimal (n) {
+    return parseFloat(n)
+}
+
